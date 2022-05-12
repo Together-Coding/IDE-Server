b159ab9d2afeada496df2c22dd2889c40e3c45bd
a2tt <usera2tt@gmail.com>
2022-05-12 15:07:56 +0900

    chore: unify error response form
    always return list of error messages even for one message

--- server/utils/response.py
+++ server/utils/response.py
@@ -7,5 +7,8 @@ def api_response(data: dict = None, status_code: int = 200):
     return JSONResponse(status_code=status_code, content=data)
 
 
-def ws_error_response(reason: str) -> dict:
+def ws_error_response(reason: str | list[str]) -> dict[str, list]:
+    if type(reason) == str:
+        reason = [reason]
+
     return dict(error=reason)
