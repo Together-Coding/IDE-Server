25f9904d1c51011209c47c2d18f1bf9d0620bd5e
a2tt <usera2tt@gmail.com>
2022-05-12 15:14:12 +0900

    fix: refine S3 object key, add missing `Key` param

--- server/helpers/s3.py
+++ server/helpers/s3.py
@@ -8,23 +8,41 @@ from configs import settings
 _s3 = boto3.client("s3", region_name="ap-northeast-2")
 
 
-def get_object(key: str, bucket: str = settings.S3_BUCKET):
-    key = key.strip("/")
-    return _s3.get_object(Bucket=bucket, Key=key)
+def _refine_key(key: str):
+    """Refine S3 object key; as the leading slash(/) is treated as filename, 
+    it should be removed.
+
+    Args:
+        key (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    return key.strip("/")
 
 
-def put_object(body: BufferedReader, key: str, bucket: str = settings.S3_BUCKET, acl="private"):
+def get_object(key: str, bucket: str | None = None):
+    if not bucket:
+        bucket = settings.S3_BUCKET
+
+    return _s3.get_object(Bucket=bucket, Key=_refine_key(key))
+
+
+def put_object(body: BufferedReader, key: str, bucket: str | None = None, acl="private"):
+    if not bucket:
+        bucket = settings.S3_BUCKET
+
     body.seek(0)
-
-    return _s3.put_object(
-        ACL=acl,
-        Body=body.read(),
-    )
+    return _s3.put_object(ACL=acl, Body=body.read(), Key=_refine_key(key))
 
 
-def is_exists(key: str, bucket: str = settings.S3_BUCKET):
+def is_exists(key: str, bucket: str | None = None):
+    if not bucket:
+        bucket = settings.S3_BUCKET
+
     try:
-        _s3.head_object(Bucket=bucket, Key=key)
+        _s3.head_object(Bucket=bucket, Key=_refine_key(key))
         return True
     except ClientError:
         return False
